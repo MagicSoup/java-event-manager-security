c55b08160d19a45d9e79cc818344cfb5e98e8065
MagicSoup <championlouis@hotmail.com>
2023-01-07 09:48:59 +0100

    Init Project - Tweak application

--- src/test/java/com/event/manager/security/EventManagerSecurityApplicationTests.java
+++ src/test/java/com/event/manager/security/EventManagerSecurityApplicationTests.java
@@ -1,12 +1,22 @@
 package com.event.manager.security;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class EventManagerSecurityApplicationTests {
 
+	@Value("${server.port}")
+	private Integer serverPort;
+
 	@Test
 	void contextLoads() {
+		assertThat(serverPort).isEqualTo(9000);
 	}
 }
